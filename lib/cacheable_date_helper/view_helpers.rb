16d53688e948979647623c49a40d1479a7471582
Murui <thomas@dippel.dk>
2011-07-18 15:51:07 +0200

    Now using .html_safe instead of raw

--- lib/cacheable_date_helper/view_helpers.rb
+++ lib/cacheable_date_helper/view_helpers.rb
@@ -8,7 +8,7 @@ module CacheableDateHelper
     # Example: cacheable_time_ago_in_words(Time.zone.now - 2 minutes) # => <span class="time_ago_in_words" data-timestamp="Mon Jul 18 12:21:12 UTC 2011" data-include-seconds="false">18 Jul 14:21</span>
     # Which will be replaced by the client-side javascript with: <span class="time_ago_in_words" data-timestamp="Mon Jul 18 12:22:11 UTC 2011" data-include-seconds="false">2 minutes ago</span>
     def cacheable_time_ago_in_words(date_with_timezone, include_seconds = false)
-   	  raw "<span class=\"time_ago_in_words\" data-timestamp=\"#{date_with_timezone.utc}\" data-include-seconds=\"#{include_seconds}\">#{I18n.l(date_with_timezone, :format => :short)}</span>"
+   	  "<span class=\"time_ago_in_words\" data-timestamp=\"#{date_with_timezone.utc}\" data-include-seconds=\"#{include_seconds}\">#{I18n.l(date_with_timezone, :format => :short)}</span>".html_safe
     end
   end
 end
